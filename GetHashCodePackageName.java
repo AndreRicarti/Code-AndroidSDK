dbc1948e02264d5396e0c4aae2d53101ff30972c
André Felipe Ricarti <a.ricarti@qiwi.brasil>
2016-12-20 11:20:04 -0200

    Code in java return hashCode Package Name

---
+++ GetHashCodePackageName.java
@@ -0,0 +1,6 @@
+//Get hashCode Package Name
+//@author Son182
+//@Date : 2016-12-20
+
+int hashCodePackageName = getApplicationContext().getPackageName().hashCode();
+Log.d("MyApp", "hash " + hashCodePackageName);
